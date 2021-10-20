aee9b5cc46b6a12fc62a0bb595dce0e5b7af7a51
AstoraymeFredy <astoraymefredy@gmail.com>
2021-10-19 22:49:52 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/spring/CleanExpertApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/spring/CleanExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
