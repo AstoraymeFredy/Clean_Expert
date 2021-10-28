91399dde258b6718bed872bafb8d9de47d3fa8e3
AstoraymeFredy <astoraymefredy@gmail.com>
2021-10-28 18:53:35 -0500

    :fix: imports

--- src/main/java/pe/edu/upc/spring/model/CleaningStaff.java
+++ src/main/java/pe/edu/upc/spring/model/CleaningStaff.java
@@ -2,7 +2,6 @@ package pe.edu.upc.spring.model;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
